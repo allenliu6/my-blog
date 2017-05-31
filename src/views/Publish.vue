71b74f1d69d30aedf8c05d879dc7a138420b3f9d
allenliu <allenliu6@yeah.net>
2017-05-31 11:13:17 +0800

    增量添加主体框架

---
+++ src/views/Publish.vue
@@ -0,0 +1,38 @@
+<template>
+    <content class="publish">
+        <textarea v-model="md" class="publish_edit" ></textarea>
+        <article v-html="markdown(md)" class="publish_show"></article>
+    </content>
+</template>
+
+<script>
+    import marked from 'marked'
+    export default {
+        data(){
+            return {
+                md: '',
+                mark: ''
+            }
+        },
+        methods: {
+            markdown(){
+                return marked(this.md)
+            }
+        }
+    }
+</script>
+
+<<style lang="postcss">
+    .publish{
+        
+
+        & .publish_edit{
+            
+        }
+
+        & .publish_show{
+
+        }
+    }
+</style>
+
